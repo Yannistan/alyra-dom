98835a3d8f94cef9d8ea6fe68945a891ed4d666a
Yannistan <tanis.ioannis@gmail.com>
2020-07-27 00:21:14 +0200

    exercises domstart

--- alyragradients/js/script.js
+++ alyragradients/js/script.js
@@ -8,9 +8,10 @@ console.log(gradients)
 
 /* dans le paragraphe #intro afficher la phrase 
 Voici une collection de ... dégradés prêts à utiliser dans vos feuilles de styles
-
 ou ... correspond au nombre de dégradés */
 
+const pIntroEl = document.getElementById("intro")
+pIntroEl.textContent = `Voici une collection de ${gradients.length} dégradés prêts à utiliser dans vos feuilles de styles`
 /* header : 
 ajouter la propriété backgroundImage au style de header
 linear-gradient(90deg, .., ..)
@@ -18,3 +19,9 @@ ou ... correspondent aux couleurs start et end d'un des objet de l'array gradien
 
 Le bouton #header-button permet de regénérer le header (=changer le dégradé)
 */
+const headerEl = document.getElementById("header")
+const linear-gradient = [ 90, "rgb(189, 195, 199)", "rgb(44, 62, 80)"]
+headerEl.style.backgroundImage = linear-gradient
+headerEl.setAttribute(
+    'style', `background-Image:${linear-gradient}`
+)
--- domstart/js/script.js
+++ domstart/js/script.js
@@ -1,1 +1,47 @@
 "use strict"
+
+// document.body.style.background = "magenta"
+/*
+document.body.style.opacity = 0
+document.body.style.transition = "ls"
+*/
+/* 1.Le contenu du timeout apparait avec transition */
+/* stTimeout crée un delai */
+
+setTimeout( () => {
+    document.body.style.opacity = 1 // style inline, force 1000
+}, 500) 
+
+/* 2.La propriete background-color de l'element
+header est aleatoire
+["hotpink", "tomato", "orange"]
+*/
+
+const headerEl = document.getElementById("header")
+console.dir(headerEl)
+const colors = ["hotpink", "tomato", "orange"]
+const randomIndexColor = Math.floor(Math.random()*colors.length)
+
+headerEl.style.backgroundColor = colors[randomIndexColor]
+
+/*
+headerEl.setAttribute(
+    'style', `background-color:${colors[randomIndexColor]}!important;`
+)
+
+/* 3 a  faire */ 
+const hellos = ["Hello", "Salut", "Hola", "Cześć"]
+  const randomIndexHellos = Math.floor(Math.random() * hellos.length)
+  const tagline = document.getElementById("tagline")
+
+  tagline.textContent = `${hellos[randomIndexHellos]}` 
+
+
+/* 4 */
+
+const pIntroEl = document.getElementById("intro-criteres")
+
+const criteres = document.querySelectorAll('#liste-criteres li')
+console.log(criteres.length)
+
+pIntroEl.textContent = `Nous avons préparés ${criteres.length} criteres`
